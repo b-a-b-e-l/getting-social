51c176fde3b0ef03481e8c9d1c1ba2ab33b31830
Maria Belen Bednarski <72574650+b-a-b-e-l@users.noreply.github.com>
2021-07-06 17:06:19 +0200

    Created login pages

--- src/components/LoginTemplate.jsx
+++ src/components/LoginTemplate.jsx
@@ -1,8 +1,9 @@
-import { Box, Typography } from '@material-ui/core';
+import { Box, Typography, Grid, Link } from '@material-ui/core';
 import StyledIcon from './StyledIcon.jsx';
+import { Link as RouterLink } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-
-const LoginTemplate = ({children, title, subtitle}) => {
+const LoginTemplate = ({children, title, subtitle, leftLink, rightLink}) => {
 return <Box alignItems="center" minHeight="100vh" display="flex" 
 flexDirection="column" justifyContent="center">
     <Box marginBottom="1em" display="flex" flexDirection="column" justifyContent="center">
@@ -11,6 +12,16 @@ flexDirection="column" justifyContent="center">
     < Typography variant="body1" > {subtitle} </Typography>
     </Box>
 {children}
+    <Grid container direction="row" justify="space-between" alignItems="flex-start" >
+      <Router>
+    <div>
+         <Link href={leftLink[1]} variant="body2"> {leftLink[0]} </Link>
+    </div>
+    <div>
+      <Link component={rightLink[1]} to={rightLink[1]} variant="body2">{rightLink[0]}</Link>
+    </div>
+    </Router>
+    </Grid>
 </Box>
 }
 
--- src/containers/login/components/LoginForm.jsx
+++ src/containers/login/components/LoginForm.jsx
@@ -4,7 +4,7 @@ import ResponsiveBox from "../../../components/ResponsiveBox"
 
 export default function LoginForm (){
   return (
-<ResponsiveBox sm={{ maxWidth: "20px" }} md={{ maxWidth:"100px" }} color="white" bgcolor="palevioletred">
+<ResponsiveBox sm={{ maxWidth:"100%" }} md={{ maxWidth:"25%" }}>
   <form  autoComplete="off">
         <TextField id="outlined-basic" label="email" variant="outlined" placeholder="email@domain.com" fullWidth style={{ marginBottom: "1em" }} />
         <TextField id="outlined-basic" label="password" variant="outlined" placeholder="*******" fullWidth style={{ marginBottom: "1em" }}/>
@@ -14,6 +14,7 @@ export default function LoginForm (){
           />
         <Button variant="contained" color="primary"> Log in </Button>
     </form>
+
   </ResponsiveBox>
   )
 }
